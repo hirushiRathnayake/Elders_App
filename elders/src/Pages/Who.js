76cdd34d0e59fe56d2b47b3cdb7bc663254474fb
hirushiRathnayake <hirushir99@gmail.com>
2024-01-23 23:43:20 +0530

    Add timeline

--- elders/src/Pages/Who.js
+++ elders/src/Pages/Who.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import '../style/Who.css';
-import me from '../Asssts/me.jpg';
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+// import SchoolIcon from "@material-ui/icons/School";
+import SchoolIcon from '@mui/icons-material/School';
+// import WorkIcon from "@material-ui/icons/Work";
 
-
-
-function Who() {
+function Experience() {
   return (
-    <div className='Who'>
-    <h1>Welcome to Hirushi's Website.</h1>
-    <img src={me} alt='My photo' width='300px' height='300px'/>
-    <p>Hello everyone, I am Hirushi Rathnayake and I am a passionate enthusiastic skilled undergraduate from University of Moratuwa, Faculty of Information Technology.
-      I schooled at Kandy Girls' High school and my home town is kandy. As a undergraduate my responsibility is to provide valuble things to world. I am a good team player and also a good team leader 
-      with solid and friendly leadership qualities. I like to learn many new things using self studies. Geeksforgeeks, Tutorials point are some tutorials which I follows and
-      Youtube tutorials also good for learn things. 
-    </p>
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+      <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2004 - 2009"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+           Thalatu-oya Junior School, Thalatu-Oya, Sri Lanka
+          </h3>
+          <p> Secondary education</p>
+        </VerticalTimelineElement>
+      <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2015"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Swarnamali Girls' College, Kandy, Sri Lanka
+          </h3>
+          <p> Secondary education</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2016 - 2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Kandy Girls' High School, Kandy, Sri Lanka
+          </h3>
+          <p> Secondary education</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - present"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            University of Moratuwa, Moratuwa, Sri Lanka
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+
+          <p> Information Technology</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
-  )
+  );
 }
 
-export default Who
+export default Experience;
