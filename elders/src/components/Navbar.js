7d6b8051cc3dacb9fb5f87bd0e28ad2fd5d41f16
hirushiRathnayake <hirushir99@gmail.com>
2024-01-23 20:55:26 +0530

    make toggle button

--- elders/src/components/Navbar.js
+++ elders/src/components/Navbar.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import '../style/Navbar.css';
-import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div className='navbar'>
-      <div className='toggleButton'>
-        <button><ReorderIcon/></button>
-        <div className='links'>
-               <Link to='/'>Home</Link>
-               <Link to='/Who'>Who</Link>
-               <Link to='/Work'>Work</Link>
-               <Link to='/Contact'>Contact</Link>
-        </div>
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
       </div>
-    
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/Who"> Who </Link>
+        <Link to="/Work"> Work </Link>
+        <Link to="/Contact"> Contact </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
